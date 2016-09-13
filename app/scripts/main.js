52f30b55e9116e19c0259b0247d31b63df5e2a4a
OUALI Aghiles Massinissa <aghiles.ouali@nassidj.com>
2016-09-13 12:58:08 +0100

    ajout de la tructure de dossier

---
+++ app/scripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+     $('.accroche').css({
+          'transform':'translate(0px,'+ wScroll / 10 +'%)'
+      });
+    });
+
+
+
+    $('nav a[href*=\\#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target
+          || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body')
+            .animate({scrollTop: targetOffset}, 1000);
+           return false;
+          }
+        }
+      });
+
+
+$("#nsc_link_modal").animatedModal();
+
+ });
